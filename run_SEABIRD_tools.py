1b4612d417cb5b90719eff5d910ac677e125ffca
Prodyut Roy <Prodyut.Roy@dfo-mpo.gc.ca>
2026-03-27 13:37:41 -0300

    Updated Toml

--- run_SEABIRD_tools.py
+++ run_SEABIRD_tools.py
@@ -1,14 +1,15 @@
 import seabird
 from seabird.cnv import fCNV
 from seabird.qc import fProfileQC
+from gsw import z_from_p
+import gsw
 
-
-
-
+print(gsw.__version__)
 cnv_file_location = "./sampledata/cnv/D900a108.cnv"
 profile = fCNV(cnv_file_location)
 
+print("Header: %s" % profile.attributes.keys())
+print("Data: %s" % profile.keys())
 
-
-df = profile.as_DataFrame()
-print(df.head())
+# df = profile.as_DataFrame()
+# print(df.head())
